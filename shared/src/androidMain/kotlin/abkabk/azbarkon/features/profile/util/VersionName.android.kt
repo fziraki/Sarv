45413438a0be89d9fe655fd7b99e2ff3430bd548
Fatemeh Ziraki <49563346+fziraki@users.noreply.github.com>
2026-08-25 19:22:56 +0330

    fix: use orEmpty() in VersionName

--- shared/src/androidMain/kotlin/abkabk/azbarkon/features/profile/util/VersionName.android.kt
+++ shared/src/androidMain/kotlin/abkabk/azbarkon/features/profile/util/VersionName.android.kt
@@ -5,5 +5,5 @@ import org.koin.core.context.GlobalContext
 
 actual fun versionName(): String {
     val context = GlobalContext.get().get<Context>()
-    return context.packageManager.getPackageInfo(context.packageName, 0).versionName ?: ""
+    return context.packageManager.getPackageInfo(context.packageName, 0).versionName.orEmpty()
 }
