d2c097bc7446a8246760d11dfd8f53e946cdb284
Fatemeh Ziraki <49563346+fziraki@users.noreply.github.com>
2026-09-14 18:29:34 +0330

    Fix iOS store URL to GitHub Releases

--- shared/src/commonMain/kotlin/abkabk/azbarkon/core/util/Constants.kt
+++ shared/src/commonMain/kotlin/abkabk/azbarkon/core/util/Constants.kt
@@ -6,5 +6,5 @@ object Constants {
     const val APP_UPDATE_CONFIG_URL =
         "https://raw.githubusercontent.com/fziraki/SarvData/main/app_update_config.json"
     const val ANDROID_STORE_URL = "https://cafebazaar.ir/app/abkabk.azbarkon"
-    const val IOS_STORE_URL = "https://cafebazaar.ir/app/abkabk.azbarkon"
+    const val IOS_STORE_URL = "https://github.com/fziraki/Sarv/releases/download/latest-ios/Sarv.ipa"
 }
